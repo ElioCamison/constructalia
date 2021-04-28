07d560ba6415d0ddcc4ac74ef92106fe196f7d87
Elio <elio.camison@okgroup.es>
2021-04-28 21:33:27 +0200

    CONS-04 #time 1h 30m Creados modelos.

---
+++ models/order/Supplier.php
@@ -0,0 +1,118 @@
+<?php
+
+require '../../config/BbConnection.php';
+
+class Supplier
+{
+    private $conn;
+
+    private $id;
+    private $name;
+    private $code;
+    private $email;
+    private $address;
+    private $phone;
+
+
+    public function __construct($name, $code, $email, $address, $phone)
+    {
+        $this->name = $name;
+        $this->code = $code;
+        $this->email = $email;
+        $this->address = $address;
+        $this->phone = $phone;
+
+        // Encontrar un método mejor de hacer esto.
+        $this->conn = BbConnection::getConnection();
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+}
